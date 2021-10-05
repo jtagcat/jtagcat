fdb838a63e5a37b6c71d4c216b428d7e3f3ab96c
jtagcat <git-514635f7@jtag.cat>
2021-10-06 00:23:15 +0300

    android udev: use sudo

--- scripts/setup/android-udev.sh
+++ scripts/setup/android-udev.sh
@@ -1,5 +1,5 @@
 # https://stackoverflow.com/a/45006231/12470046
-groupadd plugdev
+sudo groupadd plugdev
 sudo usermod -aG plugdev $USER
 echo 'ACTION=="add", SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ENV{ID_USB_INTERFACES}=="*:ff420?:*", MODE="0666", GROUP="plugdev"' | sudo tee /etc/udev/rules.d/99-android.rules
 sudo udevadm control --reload-rules
