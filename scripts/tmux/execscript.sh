2579abb168e4caf589402ea5f1e91020286c22df
jtagcat <git-514635f7@jtag.cat>
2022-01-04 16:05:57 +0000

    add execscript for flush_scrollbacks
    wraps dumb deleting and committing after each flush
    
    if dir not exist, inits one

---
+++ scripts/tmux/execscript.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+scriptloc="${1}"
+basepath="${2}"
+hostname="${3}"
+
+outdir="${basepath}/${hostname}"
+
+if [ -d "${outdir}/.git" ]; then
+	if [[ -n $(shopt -s nullglob; echo "${outdir}"/*) ]]; then
+		rm -r "${outdir}/"* # all but hidden
+	fi
+	"${scriptloc}" "${outdir}" || exit 0 # will error if no tmux session
+	git -C "${outdir}" add -- . >> /dev/null # no -q available
+	git -C "${outdir}" commit -qm'autoflush'
+else
+	mkdir "${outdir}"
+	git init "${outdir}"
+	git -C "${outdir}" config user.email "noreply@${hostname}"
+	git -C "${outdir}" config user.name "$USER"
+	git -C "${outdir}" commit -m'Initial commit' --allow-empty
+	# actual flush will happen on next execute, too minor to reuse-wrap-bla
+fi
+
--- scripts/tmux/flush_scrollbacks.sh
+++ scripts/tmux/flush_scrollbacks.sh
@@ -2,19 +2,19 @@
 set -euo pipefail
 
 # Runs capture-pane on all panes on a tmux server.
-basepath="${1}"
-# WARN: overwrites only existing panes, does not delete closed panes. Delete basepath before running for a mirror of current state.
+baseout="${1}"
+# WARN: overwrites only existing panes, does not delete closed panes. Delete baseout before running for a mirror of current state.
 
 panes="$(tmux list-panes -a -F '#{session_created}_#{session_id}/#{window_id}/#{pane_id}')"
   # a: all panes of tmux server
 grep -v '^ *#' <<< "$panes" | while IFS= read -r panepath; do
-        outpath="${basepath}/${panepath}"
+        outpath="${baseout}/${panepath}"
         mkdir -p "$(dirname "${outpath}")"
 	paneid="$(basename "${panepath}")"
-	tmux capture-pane -peNJ -S - -t "${paneid}" > "${outpath}"
+	tmux capture-pane -peJ -S - -t "${paneid}" > "${outpath}"
 	  # p: to stdout
           # e: keep escape sequences
-          # N: preserve trailing spaces
+          # unavail: N: preserve trailing spaces
           # J: preserve trailing spaces and join wrapped lines (is N redundant?)
           # S: line nr of caputre start (0 is the top of screen, negative line numbers are history): '-' is infinite negativity, everything
           # (E: end line defaults to max)
