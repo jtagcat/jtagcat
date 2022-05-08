dc29c6c971e74417b4c45c690a201f3066a71031
jtagcat <git-514635f7@jtag.cat>
2022-05-09 01:04:45 +0300

    add a5-a4doublesideFoldable.sh

---
+++ scripts/sh/a5-a4doublesideFoldable.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# AUTHOR: jtagcat
+# LICENCE:  GPL3
+
+array=( "pdfjam" "pdftk" )
+for i in "${array[@]}"; do
+    if ! command -v "$i" >/dev/null 2>&1; then
+        echo >&2 "$i not found, please install the dependancy."; 
+        exit 1 
+    fi
+done
+
+if [[ "$#" != 2 ]]; then
+  echo "usage: a5-a4doublesideFoldable.sh <input.pdf> <output.pdf>"
+  exit 1
+fi
+
+t="$(mktemp)"
+pdfjam -q --booklet true --page a4paper --landscape --nup 2x1 "$1" -o "$t"
+pdftk "$t" rotate oddDown output "$2"
