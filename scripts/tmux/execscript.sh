5358d14edfb2306560cc91479bdda252ccf875ab
jtagcat <git-514635f7@jtag.cat>
2022-07-29 18:53:31 +0300

    comment tmux store git stuff fs snapshots already provide history, and git needs maintenance

--- scripts/tmux/execscript.sh
+++ scripts/tmux/execscript.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 #TODO: implement locking with timeouts
+# edited to not use git
 
 scriptloc="${1}"
 basepath="${2}"
@@ -8,19 +9,20 @@ hostname="${3}"
 
 outdir="${basepath}/${hostname}"
 
-if [ -d "${outdir}/.git" ]; then
+#if [ -d "${outdir}/.git" ]; then
+if [ -d "${outdir}" ]; then
 	if [[ -n $(shopt -s nullglob; echo "${outdir}"/*) ]]; then
 		rm -r "${outdir}/"* # all but hidden
 	fi
 	"${scriptloc}" "${outdir}" || exit 0 # will error if no tmux session
-	git -C "${outdir}" add -- . >> /dev/null # no -q available
-	git -C "${outdir}" commit -qm'autoflush' --allow-empty || true # if nothing has changed
+#	git -C "${outdir}" add -- . >> /dev/null # no -q available
+#	git -C "${outdir}" commit -qm'autoflush' --allow-empty || true # if nothing has changed
 else
 	mkdir "${outdir}"
-	git init "${outdir}"
-	git -C "${outdir}" config user.email "noreply@${hostname}"
-	git -C "${outdir}" config user.name "$USER"
-	git -C "${outdir}" commit -m'Initial commit' --allow-empty
+#	git init "${outdir}"
+#	git -C "${outdir}" config user.email "noreply@${hostname}"
+#	git -C "${outdir}" config user.name "$USER"
+#	git -C "${outdir}" commit -m'Initial commit' --allow-empty
 	# actual flush will happen on next execute, too minor to reuse-wrap-bla
 fi
 
