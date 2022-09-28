879e55392878b5b2a765a1b63b36601df018cf3c
jtagcat <git-514635f7@jtag.cat>
2022-09-28 21:14:14 +0300

    change comments.info.json to comments_info.json
    allows using *.info.json

--- scripts/yt-dlp-pipeline-archiver/202_comments.sh
+++ scripts/yt-dlp-pipeline-archiver/202_comments.sh
@@ -10,7 +10,7 @@ for category in "$DLBASE/"*"/_indexes/known_urls.txt"; do
 	yt-dlp --config-locations "$(dirname "$0")/common.conf" \
 		--batch-file="$indexloc/comments_urls.txt" --skip-download \
 		--write-comments \
-		--output "./%(uploader).200s (%(uploader_id)s)/%(upload_date)s %(id)s.comments.%(ext)s" `# don't touch the original info.json` \
+		--output "./%(uploader).200s (%(uploader_id)s)/%(upload_date)s %(id)s.comments_%(ext)s" `# don't touch the original info.json` \
 		--force-write-archive --download-archive="$indexloc/comments.txt" \
 		--print-to-file after_video:"%(epoch)s %(webpage_url)s" "./indexes/comments_timestamps.txt" \
                 --paths home:"$DLBASE/$category" --paths temp:"$DLBASE/$category/_temp"
