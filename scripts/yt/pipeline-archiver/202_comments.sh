b4b18cda85bf76110698a6d816543196d5f098c5
jtagcat <git-12dbd862@jtag.cat>
2022-10-23 16:06:29 +0300

    yt: drop attempt of custom file ext
    the normal one is appended anyway

--- scripts/yt/pipeline-archiver/202_comments.sh
+++ scripts/yt/pipeline-archiver/202_comments.sh
@@ -11,7 +11,7 @@ for categorypath in "$DLBASE/"*; do if [ -s "$categorypath/_indexes/comments_url
 	yt-dlp --config-locations "$(dirname "$0")/common.conf" \
 		--batch-file="$categorypath/_indexes/comments_urls.txt" --skip-download \
 		--write-comments \
-		--output "%(playlist_title,uploader).200s (%(playlist_id,uploader_id)s)/%(upload_date)s %(id)s.comments_info.json" `# don't touch the original info.json` \
+		--output "%(playlist_title,uploader).200s (%(playlist_id,uploader_id)s)/%(upload_date)s %(id)s.comments.%(ext)s" `# don't touch the original info.json` \
 		--force-write-archive --download-archive="$categorypath/_indexes/comments.txt" \
 		--print-to-file after_video:"%(epoch)s %(webpage_url)s" "$categorypath/_indexes/comments_timestamps.txt" \
                 --paths home:"$DLBASE/$category" --paths temp:"$DLBASE/$category/_temp"
