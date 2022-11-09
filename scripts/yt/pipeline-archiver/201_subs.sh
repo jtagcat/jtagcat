66a947e8f0c2da130574268aa0057551d96ee12c
jtagcat <git-12dbd862@jtag.cat>
2022-11-09 18:12:27 +0200

    yt: track date of subtitles

--- scripts/yt/pipeline-archiver/201_subs.sh
+++ scripts/yt/pipeline-archiver/201_subs.sh
@@ -12,5 +12,6 @@ for categorypath in "$DLBASE/"*; do if [ -s "$categorypath/_indexes/known_urls.t
 		--batch-file="$categorypath/_indexes/known_urls.txt" --skip-download \
 		--write-subs --sub-langs all --write-auto-subs --extractor-args youtube:skip=translated_subs `# for no live chats (if currnetly live is filtered seperately): --sub-langs all,-live_chat` \
 		--force-write-archive --download-archive="$categorypath/_indexes/subs.txt" \
+		--print-to-file after_video:"%(epoch)s %(webpage_url)s" "$categorypath/_indexes/subtitle_timestamps.txt" \
                 --paths home:"$DLBASE/$category" --paths temp:"$DLBASE/$category/_temp"
 fi;done
