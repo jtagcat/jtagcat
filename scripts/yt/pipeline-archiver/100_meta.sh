1e033b803d098d13d65020ba74b5bda0e1b6eb1e
jtagcat <git-12dbd862@jtag.cat>
2022-12-11 14:21:45 +0200

    yt-pipeline: rework 100_meta tmp handling

--- scripts/yt/pipeline-archiver/100_meta.sh
+++ scripts/yt/pipeline-archiver/100_meta.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -uo pipefail
+set -euo pipefail
 shopt -s nullglob
 
 YTDLPFFMPEG="$(dirname "$0")/ffmpeg"
@@ -22,18 +22,25 @@ for file in "$INPUTS/"*; do if [ -s "$file" ]; then # has something in it
 	category="$(basename "$file" | sed 's/.txt$//' | cut -d+ -f1)"
 	mkdir -p "$DLBASE/$category/_indexes"
 
-	tempf="$(mktemp -p "$INPUTS"/../tmp)"
+	touch "$DLBASE/$category/_indexes/meta.txt" # using _staging allows for interrupts
+	cp "$DLBASE/$category/_indexes/meta.txt" "$DLBASE/$category/_indexes/meta_staging.txt"
+	
+	truncate -s 0 "$DLBASE/$category/_indexes/known_urls_staging.txt"
+
+	mkdir -p "$INPUTS""/../tmp"
 	yt-dlp --config-locations "$(dirname "$0")/common.conf" \
 		--batch-file="$INPUTS/$(basename "$file")" --skip-download \
 		--write-info-json --write-description --write-thumbnail \
-		--force-write-archive --download-archive="$DLBASE/$category/_indexes/meta.txt" \
-		--print-to-file after_video:"%(webpage_url)s" "$tempf" \
+		--force-write-archive --download-archive="$DLBASE/$category/_indexes/meta_staging.txt" \
+		--print-to-file 'after_video:%(webpage_url)s' "$DLBASE/$category/_indexes/known_urls_staging.txt" \
 		--paths home:"$DLBASE/$category" --paths temp:"$DLBASE/$category/_temp"
+	>&2 echo "yt-dlp exit $?"
 	
-	cat "$tempf" >> "$DLBASE/$category/_indexes/known_urls.txt"
-        if echo "$file" |grep -q "+comments"; then
-		cat "$tempf" >> "$DLBASE/$category/_indexes/comments_urls.txt"
-        fi
+	cat "$DLBASE/$category/_indexes/known_urls_staging.txt" >> "$DLBASE/$category/_indexes/known_urls.txt"
+	if echo "$file" |grep -q "+comments"; then
+		cat "$DLBASE/$category/_indexes/known_urls_staging.txt" >> "$DLBASE/$category/_indexes/comments_urls.txt"
+	fi
+	
+	mv "$DLBASE/$category/_indexes/meta_staging.txt" "$DLBASE/$category/_indexes/meta.txt"
 
-	rm "$tempf"
 fi; done
