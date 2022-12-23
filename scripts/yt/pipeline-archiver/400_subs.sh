b19c4836fa2270662b054a11004d7fce78dfe4bb
jtagcat <git-12dbd862@jtag.cat>
2022-12-23 21:03:06 +0200

    download only en and et subtitles

--- scripts/yt/pipeline-archiver/400_subs.sh
+++ scripts/yt/pipeline-archiver/400_subs.sh
@@ -11,7 +11,7 @@ for categorypath in "$DLBASE/"*; do if [ -s "$categorypath/_indexes/known_urls.t
 
         yt-dlp --config-locations "$(dirname "$0")/common.conf" \
 		--batch-file="$categorypath/_indexes/known_urls.txt" --skip-download \
-		--write-subs --sub-langs all --write-auto-subs --extractor-args youtube:skip=translated_subs `# for no live chats (if currnetly live is filtered seperately): --sub-langs all,-live_chat` \
+		--write-subs --sub-langs 'en.*,et.*' --write-auto-subs --extractor-args youtube:skip=translated_subs `# for no live chats (if currnetly live is filtered seperately): --sub-langs all,-live_chat` \
 		--force-write-archive --download-archive="$categorypath/_indexes/subs.txt" \
 		--print-to-file after_video:"%(epoch)s %(webpage_url)s" "$categorypath/_indexes/subtitle_timestamps.txt" \
                 --paths home:"$DLBASE/$category" --paths temp:"$DLBASE/$category/_temp"
