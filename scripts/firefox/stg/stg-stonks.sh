903805ff89fc14231f4941494f45ae16835051db
jtagcat <git-12dbd862@jtag.cat>
2024-01-24 07:52:07 +0200

    stg-stonks revamp

--- scripts/firefox/stg/stg-stonks.sh
+++ scripts/firefox/stg/stg-stonks.sh
@@ -1,22 +1,40 @@
-#!/bin/bash
-# deprecated: stonks on git now available
-set -e # Exit on error
-command -v jq tr grep >> /dev/null # Check for dependencies.
+#!/usr/bin/env bash
+set -eou pipefail
 
-#TODO: replace with find . $1
-# Takes one optional argument:
-#if [ -z ${1+x} ]; then # Is 1st argument not set?
-#    listing=$(ls *stg*.json)
-#else # Directory to look in was specified.
-    listing=$(ls $1/*stg*.json)
-#fi
-# Any furhter arguments will just be ignored.
-for i in $listing; do # For each backup file
-#     count="$(jq '.groups[].tabs[].url' "$i" | wc -l)" # Get total tab (url) count of all groups.
-#     timestamp=$(tr '~' '-' <<< $i | sed 's/^auto-//' | tr '@' '-' | cut -d- -f 3-7)
-#     date=$(cut -d- -f 1-3 <<< $timestamp)
-#     time=$(cut -d- -f 4-5 <<< $timestamp | tr '-' ':')
-#     date="$(jq -r '.autoBackupLastBackupTimeStamp' "$i")"
-#     echo "$date;$count"
-echo "$(date -d@$(jq -r '.autoBackupLastBackupTimeStamp' "$i") --utc +'%Y-%m-%d %H');$(jq '.groups[].tabs[].url' "$i" | wc -l)" &
+function stgfile {
+    timestamp="$(jq -r '.autoBackupLastBackupTimeStamp' "$1")"
+    count="$(jq '[ .groups[].tabs[] , .pinnedTabs[]? ] | length' "$1")"
+    echo "${timestamp};${count};$(basename "$1")"
+}
+
+function bakdir {
+    for file in "$1"/*.json; do
+        stgfile "$file" &
+    done
+}
+
+
+out="stg-stonks_$(date --iso-8601)"
+mkdir -p "$out"
+
+cat > "$out/command.gnuplot" << EOF
+set datafile separator ';'
+set xdata time
+set timefmt "%s"
+set title "Tabs open on desktop profiles"
+set format x "%b %y"
+
+plot 
+EOF
+truncate -s -1 "$out/command.gnuplot"
+
+for dir in "$@"; do
+    name="$(basename "$dir")"
+    dir="$(readlink -m "$dir")"
+
+    bakdir "$dir" | sort > "$out/$name"
+    printf "'%s' using 1:2 with lines title '%s'," "$name" "$name" >> "$out/command.gnuplot"
 done
+
+pushd "$out"
+gnuplot -p -c "command.gnuplot"
