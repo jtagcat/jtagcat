ba76b30a25d58a7036e90b008af905319c48a974
jtagcat <git-12dbd862@jtag.cat>
2024-08-04 09:11:10 +0300

    nonyt archivers

---
+++ scripts/yt/pipeline-archiver/999_interface_vimeo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -uo pipefail
+shopt -s nullglob
+
+DLBASE=/f/std/hhstream/archiver_notyt_vimeo
+INPUTS=/f/g/jc/jtagcat/scripts/yt/pipeline-archiver/inputs.interface_vimeo
+
+for input in "$INPUTS/"*; do if [ -s "$input" ]; then # has something in it
+	category="$(basename "$input")"
+	categorypath="$DLBASE/$category"
+	mkdir -p "$categorypath/_indexes"
+
+        echo "starting $category"
+	>&2 echo "starting $category"
+
+	yt-dlp --config-locations "$(dirname "$0")/common.conf" \
+		--batch-file="$input" \
+		--write-info-json --write-description --write-thumbnail --write-subs --write-comments \
+        --paths home:"$DLBASE/$category" --paths temp:"$DLBASE/$category/_temp" \
+		--download-archive="$categorypath/_indexes/download_archive.txt" \
+		--print-to-file 'after_video:%(webpage_url)s' "$DLBASE/$category/_indexes/after_download.txt" \
+
+	>&2 echo "yt-dlp exit $?"
+fi; done
